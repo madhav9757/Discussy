5a03ea8307f32bf5b0807f4c37dce4e73a03a13a
madhav9757 <madhavsemwal9@gmail.com>
2025-07-06 15:50:50 +0530

    fix: update cookieOptions to set sameSite attribute to 'none'

--- server/utils/token.js
+++ server/utils/token.js
@@ -24,7 +24,7 @@ export const verifyToken = (token) => {
 
 export const cookieOptions = {
   httpOnly: true,
-  sameSite: 'Strict',
+  sameSite: 'none',
   secure: process.env.NODE_ENV === 'production',
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
